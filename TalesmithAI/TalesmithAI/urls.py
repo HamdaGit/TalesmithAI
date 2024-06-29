09bfcb08d6dffdbae0243bd6cf7da9eb7f904a58
HamdaGit <raheenhamda16@gmail.com>
2024-06-30 02:57:52 +0500

    update

--- TalesmithAI/TalesmithAI/urls.py
+++ TalesmithAI/TalesmithAI/urls.py
@@ -2,11 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include, re_path  # Import include
 from django.views.generic import TemplateView
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('startpage.urls')),  # Include your app's URLs
-
+    path('', index, name='index'),
     # Serve the main HTML file for all other routes
     re_path(r'^(?!/?api/).*$', TemplateView.as_view(template_name='index.html')),
 ]
